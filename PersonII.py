50b8ab00453c51297509abbaed768aa1d864e09c
Tyjah17 <70492259+Tyjah17@users.noreply.github.com>
2022-10-28 13:08:26 -0600

    Update and rename Person.py to PersonII.py

--- PersonII.py
+++ PersonII.py
@@ -1,8 +1,10 @@
-class Person:
+# parent class for Teacher, Programmer, and Nurse
+class PersonII:
     def __init__(self, f_name, l_name, age):
         self.f_name = str(f_name)
         self.l_name = str(l_name)
         self.age = int(age)
-    # since Person class is parent class nothing much is need to be said
+
+    # Person class is parent and doesn't have any tasks so display unemployed for now
     def do_work(self):
         print(f'{self.f_name} {self.l_name} is unemployed')
