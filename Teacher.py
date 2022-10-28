1c02c65ec904952104f919956999e93f5141024a
Tyjah17 <70492259+Tyjah17@users.noreply.github.com>
2022-10-28 13:09:12 -0600

    Update Teacher.py

--- Teacher.py
+++ Teacher.py
@@ -1,12 +1,17 @@
-from Person import Person
+from PersonII import PersonII
 
 
-class Teacher(Person):
+# child class of Person class
+class Teacher(PersonII):
+    # Teacher Constructor
     def __init__(self, f_name, l_name, age, subject, grade):
+        # Person Constructor
         super().__init__(f_name, l_name, age)
+        # Teacher properties
         self.subject = str(subject)
         self.grade = int(grade)
 
+    # displays what grade level and subject teaches
     def do_work(self):
         if self.grade == 1:
             print(f'Teaches: {self.grade}st grade {self.subject}')
@@ -17,5 +22,6 @@ class Teacher(Person):
         else:
             print(f'Teaches: {self.grade}th grade {self.subject}')
 
+    # str function to display name and age
     def __str__(self):
         return f'Name: {self.f_name} {self.l_name}\nAge: {self.age}'
